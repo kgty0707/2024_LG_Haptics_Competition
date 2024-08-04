728d1313e29a241152d16aaefbf04ce8183acafd
hyunwook7120 <hyunwook7120@hanyang.ac.kr>
2024-08-04 10:56:36 +0900

    FIX: voice.py의 gpt 응답 부분

--- app/routes/voice.py
+++ app/routes/voice.py
@@ -1,5 +1,5 @@
 import os
-import openai
+from openai import OpenAI
 from pydantic import BaseModel
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from fastapi.templating import Jinja2Templates
@@ -13,7 +13,7 @@ api_key = os.getenv('GPT_API_KEY')
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-client = openai.OpenAI(api_key=api_key)
+client = OpenAI(api_key=api_key)
 
 class TranscriptionResult(BaseModel):
     text: str
@@ -39,15 +39,21 @@ async def transcribe_audio(file: UploadFile = File(...)):
         user_text = transcription['text']
         
         # GPT-3.5에게 텍스트 전달 및 응답 받기
-        gpt_response = client.audio.speech.create(
-            model="tts-1",
-            voice="alloy",
-            input=user_text
+        System_Role="당신은 고도로 숙련된 메이크업 아티스트로 친절하게 색깔을 추천해 주고 소개를 해줍니다"
+        Prompt="여름 쿨톤에 어울리는 셰이더의 색깔을 알려줘"
+        gpt_response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": System_Role},
+                {"role": "user", "content": Prompt}
+            ],
+            stream=True,
         )
+        gpt_response=gpt_response.choices[0].delta.content
         
 
         # OpenAI TTS API를 사용하여 텍스트를 음성으로 변환
-        speech_response = openai.audio.speech.create(
+        speech_response = client.audio.speech.create(
             model="tts-1",
             voice="alloy",
             input=gpt_response
